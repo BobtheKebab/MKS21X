14830b333aedeee1e3c14ae58365a8e769eed4df
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-02 05:43:56 -0500

    Added constructor

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,8 +1,16 @@
 import java.util.Iterator;
+import java.util.UnsupportedOperationException;
 
 public class SuperArrayIterator implements Iterable<String> {
 
     private SuperArray sArray;
     private int index;
+
+    public SuperArrayIterator (SuperArray array) {
+	sArray = array;
+	index = 0;
+    }
+
+    
 
 }
