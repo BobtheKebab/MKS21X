49908a0ad08263b9290b6583e9b99c96aa8ca5d6
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-03 19:40:48 -0500

    Wrote next method

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,7 +1,8 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.lang.UnsupportedOperationException;
 
-public class SuperArrayIterator implements Iterable<String> {
+public class SuperArrayIterator implements Iterator<String> {
 
     private SuperArray sArray;
     private int index;
@@ -11,13 +12,20 @@ public class SuperArrayIterator implements Iterable<String> {
 	index = 0;
     }
 
-    public boolean hasNext() {
+    public boolean hasNext () {
 	try {
 	    sArray.get(index + 1);
 	} catch (IndexOutOfBoundsException x) {
 	    return false;
 	}
 	return true;
+    }
+
+    public String next () {
+	if (hasNext()) {
+	    return sArray.get(index++);
+	}
+	throw new NoSuchElementException();
     }
 
 
