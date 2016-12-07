47967a86a949e438f390fbd4a918674deee4a9d3
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-07 05:50:53 -0500

    Started constructor and array for codes

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -3,4 +3,20 @@ public class Barcode implements Comparable<Barcode> {
     private String zip;
     private int checkDigit;
 
+    String[] codes = {"||:::", ":::||", "::|:|", "::||:", ":|::|", 
+		      ":|:|:", ":||::", "|:::|", "|::|:", "|:|::"};
+
+    public Barcode (String zip) {
+	this.zip = zip;
+	for (String num : zip) {
+	    checkDigit += Integer.parseInt(num);
+	}
+	checkDigit %= 2;
+    }
+	
+    
+    public String toString () {
+	return "dank";
+    
+
 }
