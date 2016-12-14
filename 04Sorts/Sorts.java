1b2064ca370ee1da0e54690bb9192452b4e078fe
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-14 05:51:55 -0500

    Started insertionsort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -18,6 +18,23 @@ public class Sorts {
 	}
     }
 
+    public static void insertionSort (int[] data) {
+	for (int indexNow = 1; indexNow < data.length; indexNow++) {
+	    int temp = data[indexNow];
+	    for (int count = indexNow; count > 0; count--) {
+		if (temp >= data[count]) {
+		    data[count + 1] = temp;
+		    break;
+		} else {
+		    data[count + 1] = data[count];
+		}
+	    }
+	}
+    }
+		    
+	    
+
+
     public static void main (String[] args) {
 	
 	int[] a = { 69, 100, 50};
