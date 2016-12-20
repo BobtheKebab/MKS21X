f4adc3757eb8a329119bc02d22464c8961e5aff5
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2016-12-20 05:31:36 -0500

    Wrote FtoC and CtoF

---
+++ Lab_12.20/TempChange.java
@@ -0,0 +1,11 @@
+public class TempChange {
+    
+    public static double CtoF (double temp) {
+	return (temp * 9.0/5.0) + 32.0;
+    }
+
+    public static double FtoC (double temp) {
+	return (temp - 32.0) * (5.0/9.0);
+    }
+
+}
